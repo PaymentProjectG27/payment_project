4b5254783d606722792911ed2d1f7d10c9a24e4e
sardor <Sardorshavkatillayev@gmail.com>
2023-10-11 22:54:59 +0500

    initial commit

---
+++ src/main/java/com/example/payment_project/PaymentProjectApplication.java
@@ -0,0 +1,17 @@
+package com.example.payment_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentProjectApplication.class, args);
+
+
+
+
+    }
+}
+
