8a0a878263637d40f980eeb8b467800a6c64c6cd
Muslimbek Yo'ldoshov <muslimyoldoshov41@gmail.com>
2023-10-30 18:11:38 +0500

    commit

---
+++ src/main/java/com/example/lms_prayekt/entity/teacher/TeacherRepazitory.java
@@ -0,0 +1,10 @@
+package com.example.lms_prayekt.entity.teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface TeacherRepazitory extends JpaRepository<Teacher, UUID> {
+
+}
